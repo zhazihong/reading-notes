eb669a381ccb80759cf6794ddc3b111b2724b797
hanzichi <hanzichi@outlook.com>
2017-02-07 16:01:19 +0800

    modify

--- generate.js
+++ generate.js
@@ -1,4 +1,4 @@
-// generate README.md
+// to generate README.md
 
 let fs = require("fs");
 let basePath = '编写可维护的 JavaScript';
@@ -11,9 +11,24 @@ md += '## 编写可维护的 JavaScript';
 md += '\n\n';
 
 fs.readdir(basePath, function(err, files) {
+  // 需要排序
+  let res = [];
+
   files.forEach(function(fileName) {
-    // console.log(fileName)
-    md += '- [' + fileName + '](' +baseUrl + encodeURIComponent(fileName) + ')\n';
+    let pattern = /第 (\d+) 章/;
+    let arr = pattern.exec(fileName);
+
+    res.push({
+      index: +arr[1],
+      fileName: fileName,
+      fileUrl: baseUrl + encodeURIComponent(fileName)
+    });
+  });
+
+  res.sort((a, b) => (a.index - b.index));
+
+  res.forEach((item) => {
+    md += '- [' + item.fileName + '](' + item.fileUrl + ')\n';
   });
 
   fs.writeFile(mdSrc, md, (err) => {
